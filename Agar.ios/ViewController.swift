3545ed08500bcfa8542b3e3b82f4221f528a180c
Martijn de Vos <mdmartijndevos@gmail.com>
2015-06-13 20:17:41 +0200

    Initial Commit

---
+++ Agar.ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Agar.ios
+//
+//  Created by Martijn de Vos on 13-06-15.
+//  Copyright (c) 2015 martijndevos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
